3f1c2e2cff74230fa10580ad131e51e118094320
zizu12 <tozipl@TOZIPL0>
2018-09-14 07:55:48 +0200

    Tests reviewed and commented. TODO list updated. Virtualenv files added to repo.

--- test_howdoi.py
+++ test_howdoi.py
@@ -9,6 +9,9 @@ import ctypes
 import sys
 
 from howdoi import howdoi
+"""
+    Zapytanie pyquery do parsowania html
+"""
 from pyquery import PyQuery as pq
 
 class HowdoiTestCase(unittest.TestCase):
@@ -66,6 +69,9 @@ class HowdoiTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    """
+        Ten test tak na prawde testuje pobieranie elementow z listy z danej pozycji
+    """
     def test_get_link_at_pos(self):
         self.assertEqual(howdoi.get_link_at_pos(['/questions/42/'], 1),
                          '/questions/42/')
@@ -79,7 +85,7 @@ class HowdoiTestCase(unittest.TestCase):
                          '/questions/42/')
 
     def test_bad_queries(self):
-            """ Tozipl test """
+            """ Bad query test with wrong answer """
             self.assertEqual(self.call_howdoi("Jak skompresowac plik?"), "Sorry, couldn't find any help with that topic\n", "Zle pytania daja dobra odpowiedz!")
 
     def test_answers(self):
@@ -120,22 +126,57 @@ class HowdoiTestCase(unittest.TestCase):
         self.enable_network()
 
     def test_answer_links_using_l_option(self):
+        """
+            Link do odpowiedzi zawsze zawiera questions.
+            W zwiazku z tym sprawdzamy uzycie l i wynikowany URL pasuja do tego regexa.
+        :return:
+        """
         for query in self.queries:
             response = self.call_howdoi(query + ' -l')
             self.assertNotEqual(re.match('http.?://.*questions/\d.*', response, re.DOTALL), None)
 
+    """ Test jest zbut prosty. Jezeli tak to zostawimy to testowanie -a i -l jest takie same.
+        A opcja -a wyswietal wiecej.
+        Poprawione. Jest to testowane w test_all_text.
+        Ten test jest juz nie potrzebny.
+    """
     def test_answer_links_using_all_option(self):
         for query in self.queries:
             response = self.call_howdoi(query + ' -a')
-            self.assertNotEqual(re.match('.*http.?://.*questions/\d.*', response, re.DOTALL), None)
+            self.assertNotEqual(re.match('.*Answer from http.?://.*', response, re.DOTALL), None)
 
+    """
+        Pozycja nie oznacza linka z listy linkow.
+        Odpowiedz moze miec jeden link. A pozycje sa z tego linku.
+        Test:
+            Wybrac query: dobre i ktore ma conajmniej 2 pozycje.
+            Pobrac odpowiedzi z pozycji 1 i z pozycji 2.
+            Wyniki powinny byc rozne.
+    """
     def test_position(self):
         query = self.queries[0]
         first_answer = self.call_howdoi(query)
         second_answer = self.call_howdoi(query + ' -p2')
         self.assertNotEqual(first_answer, second_answer)
 
+    def test_default_position(self):
+        """
+            Test default value for position: 1
+        :return:
+        """
+        query = self.queries[1]
+        if query:
+            first_answer = self.call_howdoi(query + " -p1")
+            second_answer = self.call_howdoi(query)
+            self.assertEqual(first_answer,second_answer)
+        self.assertEqual(first_answer,second_answer,"Lack of proper query to test.")
+
     def test_all_text(self):
+        """
+            Testowanie opcji -a
+            Daje ona co innego niz domyslna odpowiedz bez podania zadnej opcji.
+            Test drugi sprawdza czy naglowek odpowiedzi dla -a jest ok.
+        """
         query = self.queries[0]
         first_answer = self.call_howdoi(query)
         second_answer = self.call_howdoi(query + ' -a')
@@ -143,18 +184,40 @@ class HowdoiTestCase(unittest.TestCase):
         self.assertNotEqual(re.match('.*Answer from http.?://.*', second_answer, re.DOTALL), None)
 
     def test_multiple_answers(self):
+        """
+            Test sprawdza ze wyswietlenie wiekszej liczby odpowiedzi
+             jest dluzsze nie domyslne wyswietlenie pierwszej.
+        :return:
+        """
+        " Dobre zapytanie."
         query = self.queries[0]
+        " Odpowiedz do pierwszego zapytania. Domyslnie jedna odpowiedz"
         first_answer = self.call_howdoi(query)
+        " 3 Odpowiedzi do zapytania. "
         second_answer = self.call_howdoi(query + ' -n3')
+        " Nie moga byc takie same. "
         self.assertNotEqual(first_answer, second_answer)
 
     def test_unicode_answer(self):
+        """
+            Test dla 4 zapytan gdzie pytania zawieraja znaki Unicode.
+            Poprawny wnyik -> odpowiedz zwraca true
+        """
         assert self.call_howdoi('make a log scale d3')
         assert self.call_howdoi('python unittest -n3')
         assert self.call_howdoi('parse html regex -a')
         assert self.call_howdoi('delete remote git branch -a')
 
+
     def test_colorize(self):
+        """
+            Sprawdzenie czy flaga -c rzeczywiscie zwraca kolorki czy tez nie.
+            Odpowiedz bez opcji nie moze zawierac koloru.
+            Odpowiedz z opcja -c musi zawierac kolor.
+            Sprawdznie czy odpowiedz jest kolorowa czy tez nie sprowadza sie do sprawdzenie czy odpowiedz zawiera znacznik [39;
+                ktory wystepuja zawsze w kolorowej odpowiedzi.
+        :return:
+        """
         query = self.queries[0]
         normal = self.call_howdoi(query)
         colorized = self.call_howdoi('-c ' + query)
@@ -162,13 +225,23 @@ class HowdoiTestCase(unittest.TestCase):
         self.assertTrue(colorized.find('[39;') is not -1)
 
     def test_get_text_without_links(self):
+        """
+            Howdoi modul powinien zamienic htmla na wewnetrzny text pobierany z html za pomoca pyquery.
+            Po co? Po to aby zamienic htmla ze stackoverflow na normalny text.
+        """
+        """ Jest sobie text html. Z samej rzeczy zawiera znaczniki htmlowe. """
         html = '''\n  <p>The halting problem is basically a\n  formal way of asking if you can tell\n  whether or not an arbitrary program\n  will eventually halt.</p>\n  \n  <p>In other words, can you write a\n  program called a halting oracle,\n  HaltingOracle(program, input), which\n  returns true if program(input) would\n  eventually halt, and which returns\n  false if it wouldn't?</p>\n  \n  <p>The answer is: no, you can't.</p>\n'''
+        """ Za pomoca biblioteki pyquery parsujemy tego htmla """
         paragraph = pq(html)
         expected_output = '''The halting problem is basically a\n  formal way of asking if you can tell\n  whether or not an arbitrary program\n  will eventually halt.\n\n  \n  \nIn other words, can you write a\n  program called a halting oracle,\n  HaltingOracle(program, input), which\n  returns true if program(input) would\n  eventually halt, and which returns\n  false if it wouldn't?\n\n  \n  \nThe answer is: no, you can't.\n\n'''
         actual_output = howdoi.get_text(paragraph)
         self.assertEqual(actual_output, expected_output)
 
     def test_get_text_with_one_link(self):
+        """
+            Html z pojedynczym linkiem powinien byc takze poprawnie sformatowany.
+        :return:
+        """
         html = '<p>It\'s a <a href="http://paulirish.com/2010/the-protocol-relative-url/">protocol-relative URL</a> (typically HTTP or HTTPS). So if I\'m on <code>http://example.org</code> and I link (or include an image, script, etc.) to <code>//example.com/1.png</code>, it goes to <code>http://example.com/1.png</code>. If I\'m on <code>https://example.org</code>, it goes to <code>https://example.com/1.png</code>.</p>'
         paragraph = pq(html)
         expected_output = "It's a [protocol-relative URL](http://paulirish.com/2010/the-protocol-relative-url/) (typically HTTP or HTTPS). So if I'm on http://example.org and I link (or include an image, script, etc.) to //example.com/1.png, it goes to http://example.com/1.png. If I'm on https://example.org, it goes to https://example.com/1.png."
@@ -204,6 +277,10 @@ class HowdoiTestCase(unittest.TestCase):
         self.assertEqual(actual_output, expected_output)
 
     def test_get_questions(self):
+        """
+            Sprawdzenie odwrotnej operacji. Dla zadanej listy linkow zwroc liste pytan.
+        :return:
+        """
         links = ['https://stackoverflow.com/questions/tagged/cat', 'http://rads.stackoverflow.com/amzn/click/B007KAZ166', 'https://stackoverflow.com/questions/40108569/how-to-get-the-last-line-of-a-file-using-cat-command']
         expected_output = ['https://stackoverflow.com/questions/40108569/how-to-get-the-last-line-of-a-file-using-cat-command']
         actual_output = howdoi._get_questions(links)
